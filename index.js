8222d0bb74748f0ff2189709fa9a7d877c1fb9ad
alex <tte577es7@yandex.ru>
2022-10-30 12:40:30 +0300

    lesson 30 part 1

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 //Документация NODE
 //https://nodejs.org/dist/latest-v16.x/docs/api/synopsis.html#example
+
 //Импортируем плагины
 const express = require("express");
 const mysql = require("mysql")
-//Добавляем плагин multer, для работы с формами и файлами в node.js
-const multer = require('multer')
-//Настраивае, куда будем сохранять файл
-const uploadFromForm = multer({dest: 'uploads/'})
-//Устанавливаем название файла на форме
-const fileFromForm = uploadFromForm.single('MYFILE')
+
 //создадим подключение к базе данных
 // 1 - Создадим функцию-конфигурацию для подключения
 function config () {
@@ -28,31 +24,7 @@ function config () {
 const connect = mysql.createPool(config())
 //Инициализируем приложение express
 const app = express();
-//Распределяем роутеры по файлам
-require('./routes/get-all-good.js')(app, connect)
 
-/**
- * План для построения интернет магазина (что нужно добавить)
- * 
- * Базовые запросы для интерфейса магазина:
- * - Получение данных пользователя по его логину и паролю (использовать POST  запрос)
- * - Получение данных о всех товарах (использовать GET запрос)
- * - Получение данных об одном товаре (Использовать GET запрос)
- * Базовые запросы для интерфейса админки магазина:
- * - Удаление товара
- * - Добавление товара
- * - Редактирование товара
- * - Выводить список пользователей
- * - Удалять пользователя
- * - Редактировать пользователя
- * Дополнительно таблица истории купленных товаров
- * - Выводить историю
- * 
- * Сделать формы на безовом маршруте, для теста пост запросов
- * 
- * ДЗ 28 урок
- * Прописать все базовые (указанные выше) маршруты
- */
 //1 - Корневой маршрут
 //Первый базовый маршрут приложения
 app.get(
@@ -98,145 +70,13 @@ app.get(
         )
     }
 )
-//Маршрут для проверки поключения к базе данных
-app.get(
-    '/connect',
-    function(request, response){
 
-        console.log(connect);
-
-        response.send(
-            'Тестируем подключение к БД'
-        )
-
-
-    }
-)
-/**
- * Маршрут для получения оного товара:
- * Автор: Румянцев Александр
- * Описание: Возвращает JSON с одним товаром 
- * Версия: v1
- * Метод: GET
- * Пример работы с запросом:
- * Ввести в адресную строку - http://localhost:3000/get_item?id=1
- */
-app.get('/get_item', function(req, res){
-    //Получаем поле id из объекта request
-    const {id} = req.query
-    //Сформировать запрос на получение одного товара
-    //Запрос отличается от предедущего, только конструкцией WHERE ID = id
-    //Где
-    // WHERE - дословно переводиться как слово - ГДЕ
-    //Далее идет условие поле в БД ID равно id(переменная, полученная выше)
-    const sql = `SELECT * FROM goods WHERE ID=${id}`;
-    //Отправляем запрос
-    connect.query(sql, (err, result) => {
-        err ? res.send(err) : res.send(JSON.stringify(result))
-    })
-})
-/**
- * Маршрут для удаления оного товара:
- * Автор: Румянцев Александр
- * Описание: Возвращает JSON с полями, которые описывают успешное удаление товара из БД 
- * Версия: v1
- * Метод: GET
- * Пример работы с запросом:
- * Ввести в адресную строку - http://localhost:3000/del_item?id=1
- */
- app.get('/del_item', function(req, res){
-    //Получаем поле id из объекта request
-    const {id} = req.query
-    //Сформировать запрос к БД
-    //Тот же замый запрос, что и в роуте для получения одного товара, но первое ключевое слово - DELETE
-    const sql = `DELETE FROM goods WHERE ID='${id}'`;
-    connect.query(sql, (err, result) => {
-        result.ourMessage = 'Объект удален, либо его нет в БД';
-        err ? res.send(err) : res.send(JSON.stringify(result))
-    })
- }) 
-/**
- * Маршрут для добавления оного товара:
- * Автор: Румянцев Александр
- * Описание: Возвращает JSON с полями, которые описывают успешное добавление товара в БД 
- * Версия: v1
- * Метод: POST
- * Пример работы с запросом:
- */
-app.post('/add_item', fileFromForm, function(req, res){
-    //Тут не можем чистать данных с формы без дополнительных плагинов
-    //Установил плагин multer, для чтения формы и передачи файлов
-    console.log(req.body.TITLE)
-    res.send('Заглушка для добавления товара')
-})
-
-/**
- * Вспомогательный маршрут для добавления товара в БД
- * Автор: Румянцев Александр
- * Описание: Выводить форму на интерфейс для добавления товара 
- * Версия: v1
- * Метод: GET
- * Пример работы с запросом: 
- * Ввести в адресную строку - http://localhost:3000/form_add_item
- */
-app.get('/form_add_item', function(req, res){
-    res.send(
-        `
-            <h1>
-               Тестовая форма, для маршрута - add_item
-            </h1>
-            <form enctype="multipart/form-data"  action='/add_item' method='post'>
-                <input type='text' name='ID'/>
-                <input type='text' name='TITLE'/>
-                <input type='text' name='DISCR'/>
-                <input type='text' name='PRICE'/>
-                <input type='text' name='COUNT'/>
-                <input type='text' name='IMG'/>
-                <input value='Сохранить' type='submit'/>
-            </form>
-        `
-    )
-})
-/**
- * Маршрут для редактирования оного товара:
- * Автор: Румянцев Александр
- * Описание: Возвращает JSON с полями, которые описывают успешное редактирования товара в БД 
- * Версия: v1
- * Метод: POST
- * Пример работы с запросом:
- */
- app.post('/edit_item', function(req, res){
-    //Тут не можем чистать данных с формы без дополнительных плагинов
-    console.log(req)
-    console.log(req)
-    res.send('Заглушка для редактирования товара')
-})
-/**
- * Маршрут для редактирования оного товара:
- * Автор: Румянцев Александр
- * Описание: Возвращает JSON с полями, которые описывают успешное добавление товара в БД 
- * Версия: v1
- * Метод: POST
- * Пример работы с запросом:
- */
- app.get('/form_edit_item', function(req, res){
-    res.send(
-        `
-            <h1>
-               Тестовая форма, для маршрута - edit_item
-            </h1>
-            <form enctype="multipart/form-data"  action='/edit_item' method='post'>
-                <input type='text' name='ID'/>
-                <input type='text' name='TITLE'/>
-                <input type='text' name='DISCR'/>
-                <input type='text' name='PRICE'/>
-                <input type='text' name='COUNT'/>
-                <input type='text' name='IMG'/>
-                <input value='Сохранить' type='submit'/>
-            </form>
-        `
-    )
-})
+//Распределяем роутеры по файлам
+require('./routes/get-all-good.js')(app, connect)
+require('./routes/get-item.js')(app, connect)
+require('./routes/del-item.js')(app, connect)
+require('./routes/add-item.js')(app, connect)
+require('./routes/edit-item.js')(app, connect)
 
 //Начинаем прослушивать определенный порт
 app.listen(3000);
